73ac53e5f383d6714bff2a9f46344cdcc299b370
Ken Vilar <kenvilar@gmail.com>
2017-12-27 10:38:08 +0800

    Set fields value

--- register.php
+++ register.php
@@ -24,39 +24,45 @@ include 'includes/handlers/login-handler.php';
         <p>
             <?php echo $account->getErrors(Constants::$userNameLimitChars); ?>
             <label for="userName">Username</label>
-            <input type="text" name="userName" id="userName" placeholder="Please input your username here.." required>
+            <input type="text" name="userName" id="userName" placeholder="Please input your username here.."
+                   value="<?php echo $_POST['userName']; ?>" required>
         </p>
         <p>
             <?php echo $account->getErrors(Constants::$firstNameLimitChars); ?>
             <label for="firstName">First Name</label>
-            <input type="text" name="firstName" id="firstName" placeholder="Your First Name..." required>
+            <input type="text" name="firstName" id="firstName" placeholder="Your First Name..."
+                   value="<?php echo $_POST['firstName']; ?>" required>
         </p>
         <p>
             <?php echo $account->getErrors(Constants::$lastNameLimitChars); ?>
             <label for="lastName">Last Name</label>
-            <input type="text" id="lastName" name="lastName" placeholder="Your Last Name..." required>
+            <input type="text" id="lastName" name="lastName" placeholder="Your Last Name..."
+                   value="<?php echo $_POST['lastName']; ?>" required>
         </p>
         <p>
             <?php echo $account->getErrors(Constants::$emailDontMatch); ?>
             <?php echo $account->getErrors(Constants::$emailInvalid); ?>
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Your email here..." required>
+            <input type="email" id="email" name="email" placeholder="Your email here..."
+                   value="<?php echo $_POST['email']; ?>" required>
         </p>
         <p>
             <label for="confirmEmail">Confirm Email</label>
-            <input type="email" id="confirmEmail" name="confirmEmail" placeholder="Confirm your email" required>
+            <input type="email" id="confirmEmail" name="confirmEmail" placeholder="Confirm your email"
+                   value="<?php echo $_POST['confirmEmail']; ?>" required>
         </p>
         <p>
             <?php echo $account->getErrors(Constants::$passwodsDontMatch); ?>
             <?php echo $account->getErrors(Constants::$passwordLimitChars); ?>
             <?php echo $account->getErrors(Constants::$passwordNotAlphanumeric); ?>
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Your password here..." required>
+            <input type="password" id="password" name="password" placeholder="Your password here..."
+                   value="<?php echo $_POST['password']; ?>" required>
         </p>
         <p>
             <label for="confirmPassword">Confirm Password</label>
             <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password..."
-                   required>
+                   value="<?php echo $_POST['confirmPassword']; ?>" required>
         </p>
         <p>
             <button type="submit" name="registerButton">Sign Up</button>
