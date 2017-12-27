881ab718fa0d7c379d194435e15b176f322e4312
Ken Vilar <kenvilar@gmail.com>
2017-12-27 09:41:00 +0800

    Styling errors

--- register.php
+++ register.php
@@ -15,6 +15,7 @@ include 'includes/handlers/login-handler.php';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Miozika</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
