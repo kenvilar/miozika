d3f3e4c4111a775fb5330c35defcc0b6fbac0ac1
Ken Vilar <kenvilar@gmail.com>
2017-12-30 10:23:17 +0800

    Implement DB connection

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+ob_start();
+$timezone = date_default_timezone_set('Asia/Hong_Kong');
+$con = mysqli_connect('localhost', 'adminuser', 'adminpassword', 'miozika');
+
+if (mysqli_connect_errno()) {
+    echo 'Failed to connect: ' . mysqli_connect_errno();
+}
--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 
+include 'includes/config.php';
 include "includes/classes/Constants.php";
 include 'includes/classes/Account.php';
 $account = new Account();
