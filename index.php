fd70eb322cb211cf5362ec53589c50c0d7bcfcea
Ken Vilar <kenvilar@gmail.com>
2018-01-07 15:54:00 +0800

    Add player buttons

--- index.php
+++ index.php
@@ -21,11 +21,42 @@ if (isset($_SESSION['is_user_logged_in'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Miozika</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
 <div class="miozika-wrapper">
     <h1>Welcome to Miozika</h1>
+</div>
+
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+        <div class="nowPlayingLeft"></div>
+        <div class="nowPlayingCenter">
+            <div class="content player-controls">
+                <div class="buttons">
+                    <button class="controlButton shuffle" title="Shuffle Button">
+                        <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                    </button>
+                    <button class="controlButton previous" title="Previous Button">
+                        <img src="assets/images/icons/previous.png" alt="previous">
+                    </button>
+                    <button class="controlButton play" title="Play Button">
+                        <img src="assets/images/icons/play.png" alt="play">
+                    </button>
+                    <button class="controlButton next" title="Next Button">
+                        <img src="assets/images/icons/next.png" alt="next">
+                    </button>
+                    <button class="controlButton repeat" title="Repeat Button">
+                        <img src="assets/images/icons/repeat.png" alt="repeat">
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="nowPlayingRight"></div>
+    </div>
 </div>
 
 </body>
