e0ea536e108009c5feaf17ed63958db67c00c244
Ken Vilar <kenvilar@gmail.com>
2018-01-17 18:59:11 +0800

    Organize layout structure

---
+++ includes/layouts/navbarContainer.php
@@ -0,0 +1,29 @@
+<?php
+/* Navbar Container */
+?>
+
+<div id="navBarContainer">
+    <nav class="navBar">
+        <a href="/" class="logo">
+            <img src="assets/images/icons/logo.png" alt="Logo">
+        </a>
+        <div class="group">
+            <div class="navItem">
+                <a href="search.php" class="navItemLink">Search
+                    <img src="assets/images/icons/search.png" alt="Search" class="icon">
+                </a>
+            </div>
+        </div>
+        <div class="group">
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+            <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+            </div>
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">Song Artist</a>
+            </div>
+        </div>
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -30,97 +30,14 @@ if (isset($_SESSION['is_user_logged_in'])) {
 <div id="mainContainer">
 
     <div id="topContainer">
-        <div id="navBarContainer">
-            <nav class="navBar">
-                <a href="/" class="logo">
-                    <img src="assets/images/icons/logo.png" alt="Logo">
-                </a>
-                <div class="group">
-                    <div class="navItem">
-                        <a href="search.php" class="navItemLink">Search
-                            <img src="assets/images/icons/search.png" alt="Search" class="icon">
-                        </a>
-                    </div>
-                </div>
-                <div class="group">
-                    <div class="navItem">
-                        <a href="browse.php" class="navItemLink">Browse</a>
-                    </div>
-                    <div class="navItem">
-                        <a href="yourMusic.php" class="navItemLink">Your Music</a>
-                    </div>
-                    <div class="navItem">
-                        <a href="profile.php" class="navItemLink">Song Artist</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <?php
+        include 'includes/layouts/navbarContainer.php';
+        ?>
     </div>
 
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div class="nowPlayingLeft">
-                <div class="content">
-                <span class="albumLink">
-                    <img src="//i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtWork" alt="album">
-                </span>
-                    <div class="trackInfo">
-                    <span class="trackName">
-                        <span>Song Title</span>
-                    </span>
-                        <span class="artistName">
-                        <span>Song Artist</span>
-                    </span>
-                    </div>
-                </div>
-            </div>
-            <div class="nowPlayingCenter">
-                <div class="content player-controls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Shuffle Button">
-                            <img src="assets/images/icons/shuffle.png" alt="shuffle">
-                        </button>
-                        <button class="controlButton previous" title="Previous Button">
-                            <img src="assets/images/icons/previous.png" alt="previous">
-                        </button>
-                        <button class="controlButton play" title="Play Button">
-                            <img src="assets/images/icons/play.png" alt="play">
-                        </button>
-                        <button class="controlButton pause" title="Pause Button">
-                            <img src="assets/images/icons/pause.png" alt="pause">
-                        </button>
-                        <button class="controlButton next" title="Next Button">
-                            <img src="assets/images/icons/next.png" alt="next">
-                        </button>
-                        <button class="controlButton repeat" title="Repeat Button">
-                            <img src="assets/images/icons/repeat.png" alt="repeat">
-                        </button>
-                    </div>
-                    <div class="playbackBar">
-                        <span class="progressTime current">0.00</span>
-                        <div class="progressBar">
-                            <div class="middle-align progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                    </div>
-                </div>
-            </div>
-            <div class="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume Button">
-                        <img src="assets/images/icons/volume.png" alt="Volume Button">
-                    </button>
-                    <div class="progressBar">
-                        <div class="middle-align progressBarBg">
-                            <div class="progress"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+    include 'includes/layouts/nowPlayingBarContainer.php';
+    ?>
 
 </div>
 
