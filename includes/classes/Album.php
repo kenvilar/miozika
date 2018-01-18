a37d4e772fc985ea992be62c90c5955ea52565e5
Ken Vilar <kenvilar@gmail.com>
2018-01-18 19:02:54 +0800

    Create Album class

---
+++ includes/classes/Album.php
@@ -0,0 +1,39 @@
+<?php
+
+class Album {
+    private $con;
+    private $id;
+    private $title;
+    private $artistId;
+    private $genreId;
+    private $artWorPath;
+
+    public function __construct($con, $id) {
+        $this->con = $con;
+        $this->id = $id;
+
+        $albumQuery = mysqli_query($this->con, "SELECT * FROM albums WHERE id='$this->id'");
+        $album = mysqli_fetch_assoc($albumQuery);
+
+        $this->title = $album['title'];
+        $this->artistId = $album['artist'];
+        $this->genreId = $album['genre'];
+        $this->artWorkPath = $album['artworkPath'];
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getArtist() {
+        return new Artist($this->con, $this->artistId);
+    }
+
+    public function getGenre() {
+        return $this->genreId;
+    }
+
+    public function getArtWorkPath() {
+        return $this->artWorkPath;
+    }
+}
