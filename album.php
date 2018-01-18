d72e22d497ed7cf9a7eb3952a86524cbc42fb750
Ken Vilar <kenvilar@gmail.com>
2018-01-18 19:09:03 +0800

    getters

--- album.php
+++ album.php
@@ -11,7 +11,16 @@ endif;
 $album = new Album($con, $albumId);
 $artist = $album->getArtist();
 
-echo $album->getTitle() . '<br>';
-echo $artist->getName() . '<br>';
+//Get the Album Title
+$albumTitle = $album->getTitle();
+
+//Get the Album Artist Name
+$artistName = $artist->getName();
+
+//Get the Album Genre
+//echo $album->getGenre();
+
+//Get the Album Artwork Image
+$albumUrl = $album->getArtWorkPath();
 
 include 'includes/layouts/footer.php'; ?>
