8cfa90d3f4af3023ec72524612c36f43b9cc4073
Ken Vilar <kenvilar@gmail.com>
2018-01-22 00:25:26 +0800

    minor change

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -9,7 +9,8 @@ include 'includes/layouts/nowPlayingBarContainer.php';
 
 </div> <!--#mainContainer"-->
 
-<script src="./assets/js/jquery.min.js"></script>
-<script src="./assets/js/main.js"></script>
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/main.js"></script>
+
 </body>
 </html>
--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['is_user_logged_in'])) {
     <title>Welcome to Miozika</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
 
