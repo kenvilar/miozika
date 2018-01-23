cb4925577654bbb2478651de247f4cba237e76ca
Ken Vilar <kenvilar@gmail.com>
2018-01-23 14:06:48 +0800

    Format audio duration; Display and format time remaining

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,38 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(timeInSeconds) {
+	var seconds = Math.round(timeInSeconds);
+	var minutes = Math.floor(seconds / 60);
+	seconds = seconds - (minutes * 60);
+
+	var finalminutes;
+	var finalSeconds;
+
+	//Add prefix 0 if minutes and seconds are less than 0
+	if (minutes < 10) {
+		finalminutes = '0' + minutes;
+	} else {
+		finalminutes = minutes;
+	}
+
+	if (seconds < 10) {
+		finalSeconds = '0' + seconds;
+	} else {
+		finalSeconds = seconds;
+	}
+
+	return finalminutes + ':' + finalSeconds;
+}
+
 function Audio() {
 	this.currentPlaying = null;
 	this.audio = document.createElement('audio');
+
+	this.audio.addEventListener('canplay', function () {
+		var songDuration = formatTime(this.duration);
+		$('.progressTime.remaining').text(songDuration);
+	});
 
 	this.setTrack = function (track) {
 		this.currentPlaying = track;
