b574b62d4d4b0dbf63b3af4faf584c4a60385af1
Ken Vilar <kenvilar@gmail.com>
2018-01-29 20:18:57 +0800

    Update style expanded and make it minified

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['userLoggedIn'])) {
     <title>Welcome to Miozika</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.min.css">
 
     <script src="/assets/js/jquery.min.js"></script>
     <script src="/assets/js/main.js"></script>
