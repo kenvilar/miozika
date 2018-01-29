2069cc1ab4cee59829a1cee6684d3561b77bbbc3
Ken Vilar <kenvilar@gmail.com>
2018-01-29 16:31:15 +0800

    Remove content from index.php and connect to browse.php

--- index.php
+++ index.php
@@ -5,32 +5,4 @@ include 'includes/includedFiles.php';
 
 ?>
 
-    <h1 class="heading-big">You Might Also Like</h1>
-    <div class="gridViewContainer">
-
-        <?php
-        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-        while ($row = mysqli_fetch_assoc($albumQuery)) :
-            $artWorkPath = $row['artworkPath'];
-            $title = $row['title'];
-            $id = $row['id'];
-            ?>
-
-            <div class="gridViewItem">
-                <span role="link" tabindex="0" onclick="openPage('album.php?id=<?php echo $id; ?>')">
-                    <img src="<?php echo $artWorkPath; ?>" alt="image">
-                    <div class="gridViewInfo">
-                        <div class="name"><?php echo $title; ?></div>
-                    </div>
-                </span>
-            </div>
-
-            <?php
-        endwhile;
-        ?>
-
-    </div>
-
-<?php
-//include 'includes/layouts/footer.php';
+<script>openPage('browse.php')</script>
