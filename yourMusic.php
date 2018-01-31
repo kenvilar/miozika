b61322f6d4b8fbf1df27d502fb54831d2fc45e43
Ken Vilar <kenvilar@gmail.com>
2018-01-31 23:47:54 +0800

    Create yourMusic page template and its initial styling

---
+++ yourMusic.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'includes/includedFiles.php';
+
+?>
+
+<div id="your-music-wrapper">
+
+    <div class="playlists-container">
+        <div class="grid-view-container">
+            <h2 class="text-center">PLAYLISTS</h2>
+            <div class="button-items">
+                <button class="btn btn-green center-block">NEW PLAYLIST</button>
+            </div>
+        </div>
+    </div>
+
+</div>
