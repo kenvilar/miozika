b7da07919a875890c2430b21e7803d1277e1d089
Ken Vilar <kenvilar@gmail.com>
2018-01-31 16:32:33 +0800

    Implement search script

--- search.php
+++ search.php
@@ -14,3 +14,18 @@ if (isset($_GET['term'])) {
     <h4>Search for an artist, album or song</h4>
     <input type="search" class="search-input" value="<?php echo $term; ?>" placeholder="Start typing...">
 </div>
+
+<script>
+	$(function () {
+		var timer;
+
+		$('.search-input').keyup(function () {
+			clearTimeout(timer);
+
+			timer = setTimeout(function () {
+				var val = $('.search-input').val();
+				openPage('search.php?term=' + val);
+			}, 2000);
+		});
+	});
+</script>
