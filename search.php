1fa72fae843b921ceefd8720f8fa463333c99365
Ken Vilar <kenvilar@gmail.com>
2018-01-31 17:04:03 +0800

    Display the songs query when search is not empty

--- search.php
+++ search.php
@@ -11,7 +11,7 @@ if (isset($_GET['term'])) {
 ?>
 
 <div class="search-container">
-    <h4>Search for an artist, album or song</h4>
+    <h4>Search for an Artist, Song, Album or Playlist</h4>
     <input type="search" class="search-input" value="<?php echo $term; ?>" placeholder="Start typing..."
            onfocus="var val=this.value; this.value=''; this.value= val;">
 </div>
@@ -32,3 +32,60 @@ if (isset($_GET['term'])) {
 		});
 	});
 </script>
+
+<div class="trackListContainer border-bottom">
+    <h2 class="text-center">SONGS</h2>
+    <ul class="track-list">
+        <?php
+
+        //Search Songs Query
+        $songsQuery = mysqli_query($con, "SELECT id FROM songs WHERE title LIKE '%$term%'");
+
+        if (mysqli_num_rows($songsQuery) == 0) {
+            echo '<span class="no-results">No songs found matching "' . $term . '".</span>';
+        }
+
+        $songIdArr = [];
+        $i = 1;
+        while ($row = mysqli_fetch_assoc($songsQuery)) {
+            array_push($songIdArr, $row['id']);
+
+            $albumSong = new Song($con, $row['id']);
+            $albumArtist = $albumSong->getArtist();
+            $songtitle = $albumSong->getTitle();
+            $songArtist = $albumSong->getArtist()->getName();
+            $songDuration = $albumSong->getDuration();
+
+            ?>
+
+            <li class="trackListRow">
+                <div class="trackCount">
+                    <img class="play" src="assets/images/icons/play-white.png" alt="image"
+                         onclick="setTrack('<?php echo $albumSong->getId(); ?>', tempPlaylist, true);">
+                    <span class='trackNumber'><?php echo $i; ?></span>
+                </div>
+                <div class="trackInfo">
+                    <span class='trackName'><?php echo $songtitle; ?></span>
+                    <span class='artistName'><?php echo $songArtist; ?></span>
+                </div>
+                <div class="trackOptions">
+                    <img class="optionsButton" src="assets/images/icons/more.png" alt="image">
+                </div>
+                <div class="trackDuration">
+                    <span class="duration"><?php echo $songDuration; ?></span>
+                </div>
+            </li>
+
+            <?php
+
+            $i++;
+        }
+        ?>
+
+        <script>
+			var tempSongIds;
+			tempSongIds = '<?php echo json_encode($songIdArr); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+        </script>
+    </ul>
+</div>
