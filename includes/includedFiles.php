eb72007019934b589ce7610a69ddcc8a56ec6173
Ken Vilar <kenvilar@gmail.com>
2018-02-02 22:19:54 +0800

    Fix get param

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -7,8 +7,8 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     include 'includes/classes/Album.php';
     include 'includes/classes/Song.php';
 
-    if (isset($_GET['userLoggedIn'])) {
-        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    if (isset($_GET['loggedInUser'])) {
+        $userLoggedIn = new User($con, $_GET['loggedInUser']);
     } else {
         echo 'Username var defined. Check the openPage() function in main.js';
     }
