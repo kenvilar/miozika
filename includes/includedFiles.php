9ec90462af23c58975faf02e8d872186287a34ce
Ken Vilar <kenvilar@gmail.com>
2018-02-02 22:19:20 +0800

    Register User class in includedFiles.php

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -2,9 +2,16 @@
 
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     include "includes/config.php";
+    include 'includes/classes/User.php';
     include 'includes/classes/Artist.php';
     include 'includes/classes/Album.php';
     include 'includes/classes/Song.php';
+
+    if (isset($_GET['userLoggedIn'])) {
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    } else {
+        echo 'Username var defined. Check the openPage() function in main.js';
+    }
 } else {
     include 'includes/layouts/header.php';
     include 'includes/layouts/footer.php';
