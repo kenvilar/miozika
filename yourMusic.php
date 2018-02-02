6e746eeb91e5fad1bbf7f3f6aa5081922a1a6208
Ken Vilar <kenvilar@gmail.com>
2018-02-02 22:34:19 +0800

    Query playlists and display the result

--- yourMusic.php
+++ yourMusic.php
@@ -10,8 +10,34 @@ include 'includes/includedFiles.php';
         <div class="grid-view-container">
             <h2 class="text-center">PLAYLISTS</h2>
             <div class="button-items">
-                <button class="btn btn-green center-block" onclick="createPlaylist()">NEW PLAYLIST</button>
+                <button class="btn btn-green center-block" onclick="createPlaylist();">NEW PLAYLIST</button>
             </div>
+
+            <?php
+            $userLoggedIn = $userLoggedIn->getName(); //This was defined in includedFiles.php
+
+            $playlistQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$userLoggedIn'");
+
+            if (mysqli_num_rows($playlistQuery) === 0) {
+                echo '<div class="no-results text-center">No playlists found. Please create one.</div>';
+            }
+
+            while ($row = mysqli_fetch_assoc($playlistQuery)) :
+                $name = $row['name'];
+                $owner = $row['owner'];
+                $dateCreated = $row['dateCreated'];
+                ?>
+
+                <div class="gridViewItem">
+                    <div class="gridViewInfo">
+                        <div class="name"><?php echo $name; ?></div>
+                    </div>
+                </div>
+
+                <?php
+            endwhile;
+            ?>
+
         </div>
     </div>
 
