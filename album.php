7d94612664a11f9db252d26e94f3288504464361
Ken Vilar <kenvilar@gmail.com>
2018-02-07 15:03:23 +0800

    Create and style option menu;

--- album.php
+++ album.php
@@ -29,62 +29,69 @@ $numOfSongs = $album->getNumberOfSongs();
 
 ?>
 
-<div class="entityInfo">
-    <div class="leftSection">
-        <img src="<?php echo $albumUrl; ?>" alt="image">
+    <div class="entityInfo">
+        <div class="leftSection">
+            <img src="<?php echo $albumUrl; ?>" alt="image">
+        </div>
+        <div class="rightSection">
+            <h2><?php echo $albumTitle; ?></h2>
+            <p>By <?php echo $artistName; ?></p>
+            <p><?php echo $numOfSongs; ?> songs</p>
+        </div>
     </div>
-    <div class="rightSection">
-        <h2><?php echo $albumTitle; ?></h2>
-        <p>By <?php echo $artistName; ?></p>
-        <p><?php echo $numOfSongs; ?> songs</p>
-    </div>
-</div>
 
-<div class="trackListContainer">
-    <ul class="track-list">
-        <?php
-        $songIdArr = $album->getSongIds();
-        $i = 1;
-        foreach ($songIdArr as $songId) {
-            $albumSong = new Song($con, $songId);
-            $albumArtist = $albumSong->getArtist();
-            $songtitle = $albumSong->getTitle();
-            $songArtist = $albumSong->getArtist()->getName();
-            $songDuration = $albumSong->getDuration();
+    <div class="trackListContainer">
+        <ul class="track-list">
+            <?php
+            $songIdArr = $album->getSongIds();
+            $i = 1;
+            foreach ($songIdArr as $songId) {
+                $albumSong = new Song($con, $songId);
+                $albumArtist = $albumSong->getArtist();
+                $songtitle = $albumSong->getTitle();
+                $songArtist = $albumSong->getArtist()->getName();
+                $songDuration = $albumSong->getDuration();
 
+                ?>
+
+                <li class="trackListRow">
+                    <div class="trackCount">
+                        <img class="play" src="assets/images/icons/play-white.png" alt="image"
+                             onclick="setTrack('<?php echo $albumSong->getId(); ?>', tempPlaylist, true);">
+                        <span class='trackNumber'><?php echo $i; ?></span>
+                    </div>
+                    <div class="trackInfo">
+                        <span class='trackName'><?php echo $songtitle; ?></span>
+                        <span class='artistName'><?php echo $songArtist; ?></span>
+                    </div>
+                    <div class="trackOptions">
+                        <img class="optionsButton" src="assets/images/icons/more.png" alt="image">
+                    </div>
+                    <div class="trackDuration">
+                        <span class="duration"><?php echo $songDuration; ?></span>
+                    </div>
+                </li>
+
+                <?php
+
+                $i++;
+            }
             ?>
 
-            <li class="trackListRow">
-                <div class="trackCount">
-                    <img class="play" src="assets/images/icons/play-white.png" alt="image"
-                         onclick="setTrack('<?php echo $albumSong->getId(); ?>', tempPlaylist, true);">
-                    <span class='trackNumber'><?php echo $i; ?></span>
-                </div>
-                <div class="trackInfo">
-                    <span class='trackName'><?php echo $songtitle; ?></span>
-                    <span class='artistName'><?php echo $songArtist; ?></span>
-                </div>
-                <div class="trackOptions">
-                    <img class="optionsButton" src="assets/images/icons/more.png" alt="image">
-                </div>
-                <div class="trackDuration">
-                    <span class="duration"><?php echo $songDuration; ?></span>
-                </div>
-            </li>
+            <script>
+				var tempSongIds;
+				tempSongIds = '<?php echo json_encode($songIdArr); ?>';
+				tempPlaylist = JSON.parse(tempSongIds);
+            </script>
+        </ul>
+    </div>
 
-            <?php
-
-            $i++;
-        }
-        ?>
-
-        <script>
-			var tempSongIds;
-			tempSongIds = '<?php echo json_encode($songIdArr); ?>';
-			tempPlaylist = JSON.parse(tempSongIds);
-        </script>
-    </ul>
-</div>
+    <nav class="optionsMenu">
+        <input type="hidden" class="songId">
+        <div class="item">Add to playlist</div>
+        <div class="item">Item 2</div>
+        <div class="item">Item 3</div>
+    </nav>
 
 <?php
 
